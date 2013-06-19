ac5f629138b8fa266caffe81804c3216ddc808f2
Justin Ma <kerrigell@gmail.com>
2013-06-19 09:16:08 +0800

    Signed-off-by: Justin Ma <kerrigell@gmail.com>

--- node.py
+++ node.py
@@ -109,7 +109,7 @@ class Server(object):
                 env.eagerly_disconnect=True
                 env.abort_on_prompts=True
                 env.warn_only=True
-                return Server._print_result(run(cmd,shell=False),)
+                return Server._print_result(run(cmd,shell=False),prefix=False)
         except NetworkError,e:
             traceback.print_exc()
            # print '%s Error: #%d %s' % (target.address, e.args[0], e.args[1])
@@ -130,7 +130,7 @@ class Server(object):
             pass
         if result.succeeded:
             if len(result):
-                puts(yellow("%sReturnCode:%s" % result.return_code if code <> -99 else '') 
+                puts(yellow("%s ReturnCode:%s" % (str(self),result.return_code if code <> -99 else '')) 
                             + green('\n' + result)
                             ,show_prefix=prefix)
                 if hopevalue and result != hopevalue:
