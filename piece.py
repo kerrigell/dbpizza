3635cda02a706f9f79945a74442fa209903963b9
Chris Phang <penghaiyang@CY4803.cyou-inc.com>
2013-07-03 17:56:33 +0800

    Modified class knife in piece.py

--- piece.py
+++ piece.py
@@ -9,7 +9,21 @@ from dbi import session
 from node import Server
 
 class piece(Server):
-    pass
+    self.knife = aaa
+    def breed(self, kinfe = self.knife):
+    '''依据自身.dbid值，繁殖子节点：返回子嗣数量'''
+    if not (self.childs is None) and len(self.childs)>0:
+        return len(self.childs)
+    result=session.query(t_server)
+    for col, value in knife:
+        result = result.filter(t_server.:col == ":value")
+    result = result.all()
+    if result is None or len(result)==0:
+        self.childs={}
+        return 0
+    for i in result:
+        self.add_child(Server(i.id))
+    return len(self.childs)
 
 class knife(object):
     def __init__(self,*keywords):
@@ -17,7 +31,7 @@ class knife(object):
        self.words = self._all_words()
 
     def _all_words(self):
-        words = []
+        words = {}
         words += self._get_word(t_server.region)
         words += self._get_word(t_server.product)
         words += self._get_word(t_server.role)
@@ -26,12 +40,12 @@ class knife(object):
         words += self._get_word(t_server.os_type)
         words += self._get_word(t_server.os_release)
         words += self._get_word(t_server.os_arch)
-        words += ['reserve']
+        words += [('reserve',t_server.is_reserve)]
+        words = dict(words)
         return words
 
     def _get_word(self, col):
         ret = session.query(col).filter(col != None).distinct().all()
-        return [ str(x[0]) for x in ret ]
-
+        return [ (str(x[0]),col) for x in ret ]
 
 
