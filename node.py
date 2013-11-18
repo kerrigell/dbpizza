46094d39e2f10402702567958d0e505e716e8eb1
Justin Ma <kerrigell@gmail.com>
2013-11-18 19:55:04 +0800

    nagios

--- node.py
+++ node.py
@@ -1671,8 +1671,20 @@ class Crontab(object):
             print '  %50s' % ('Collected the number of crontab:%s' % count)
     def list(self):
         ripsec=self._get_dbinfo(self.server.dbid)
-        print "%5s%5s%20s%5s%5s%5s %100s%10s%5s  %s" % ("id","min",'hou','day','mon','wee','process','user','status','description')
+        print "%5s%5s%10s%5s%5s%5s %100s%10s%5s  %s" % ("id","min",'hou','day','mon','wee','process','user','status','description')
         for i in ripsec:
-            print "%5s%5s%20s%5s%5s%5s %100s%10s%5s  %s" % (i.id,i.pminute,i.phour,i.pday,i.pmonth,i.pweek,i.process,i.status,i.user,i.description)        
+            print "%5s%5s%10s%5s%5s%5s %100s%10s%5s  %s" % (i.id,i.pminute,i.phour,i.pday,i.pmonth,i.pweek,i.process,i.status,i.user,i.description)  
+            
+    def delete(self,dbid=None):
+        '''cat /var/spool/cron/root'''
+        pass
+    def disable(self,dbid=None):
+        pass
+    def enable(self,dbid=None):
+        pass
+    def create(self):
+        pass
+    def change_description(self,dbid):
+        pass
         
     
